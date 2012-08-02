861c6e4e1a9f13cf25c6c1e2725dfcfa4eed85c7
AlexWilliam108 <alexwilliamster@gmail.com>
2012-08-02 17:42:37 -0300

    Adicionado animação delete field

--- js/script.js
+++ js/script.js
@@ -4,13 +4,30 @@
 	Alex_William108
 */
 
-$(function() {
+/*$(function() {
 	$('label').each(function() {
 		$(this).find('.more-inputs').click(function(e) {
 			e.preventDefault();
-			$(this).next().css({'display' : 'block'}).closest('label').css({'height' : 'auto'})
+			$(this).next().css({'display' : 'block'}).closest('label').animate({'height' : '300px'})
 			.find('.more-inputs, .input-container').clone().insertAfter('.input-container');
 			$('.input-container:last-child').css({'display' : 'none'});
 		});
 	});
+});*/
+
+$(function() {
+	$('label').each(function() {
+		$(this).find('.more-inputs').click(function(e) {
+			e.preventDefault();
+			if ($(this).has('delete') && ('telefonos')) {
+				$(this).css({'background-position' : '66px 10px'}).attr('title', 'Eliminar Teléfono');	
+			}
+			$(this).addClass('delete').next().css({'display' : 'block'})
+			.closest('label').css({'height' : 'auto'})
+		});
+	});
+});
+
+$(function() {
+	
 });
